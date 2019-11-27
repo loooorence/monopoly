c9ba300035dc11c12cb9d9da8512cc20789ed47b
Elizabeth Figal <lizziefigal@gmail.com>
2019-11-26 21:22:56 -0800

    Implement Queue

--- src/main/java/monopoly/util/Queue.java
+++ src/main/java/monopoly/util/Queue.java
@@ -1,16 +1,28 @@
 package monopoly.util;
 
+import java.util.LinkedList;
+
 public class Queue<T> {
 
-    public void enqueue(T data) {
+    private LinkedList<T> dataList;
 
+    public Queue() {
+        this.dataList = new LinkedList<>();
+    }
+
+    public void enqueue(T data) {
+        dataList.addLast(data);
     }
 
     public T dequeue() {
-        return null;
+        return dataList.removeFirst();
     }
 
     public boolean isEmpty() {
-        return true;
+        return dataList.isEmpty();
+    }
+
+    public int size() {
+        return dataList.size();
     }
 }
